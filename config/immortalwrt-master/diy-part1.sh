5284dafcf6f15b16e42270a1dfb2a2cb71b3328a
ab45300 <130231700+ab45300@users.noreply.github.com>
2025-06-16 00:14:21 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -7,8 +7,8 @@
 #========================================================================================================================
 
 # Add a feed source
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '1i src-git kiddin9 https://github.com/kiddin9/kwrt-packages' feeds.conf.default
+# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
