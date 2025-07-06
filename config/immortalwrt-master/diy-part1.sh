7b360a1971f7d073661db4bf2a887872e5fa7e67
ab45300 <130231700+ab45300@users.noreply.github.com>
2025-07-07 01:38:53 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -10,8 +10,8 @@
 # sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i '3i turboacc https://github.com/chenmozhijin/turboacc.git;luci' feeds.conf.default
-git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-git clone https://github.com/imapproxy/luci-app-turboacc.git package/turboacc
+# git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+# git clone https://github.com/imapproxy/luci-app-turboacc.git package/turboacc
 
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
